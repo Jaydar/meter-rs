21e587b3d19ae337542d23b367a9f49faa166879
Jaydar <Jaydar@Outlook.com>
2026-07-03 21:00:43 +0800

    up: fluent

--- build.rs
+++ build.rs
@@ -11,8 +11,8 @@ fn main() {
             winresource::WindowsResource::new().compile().unwrap();
         }
     }
-
-    let conf = slint_build::CompilerConfiguration::default().with_style("cosmic".to_owned());
+    // fluent material cupertino cosmic qt native
+    let conf = slint_build::CompilerConfiguration::default().with_style("fluent".to_owned());
     slint_build::compile_with_config("ui/page/app.slint", conf).unwrap();
     slint_build::print_rustc_flags().unwrap();
 }
